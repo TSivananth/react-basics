3a4a0799eebb7f3e9259b53e2d75626f126420d5
Tharshika Sivananthan <tharshikasivananthan@Equals-T-MacBook-Pro.local>
2023-09-29 09:27:41 +0100

    order by

--- my-app/src/List.js
+++ my-app/src/List.js
@@ -4,15 +4,15 @@ import _ from 'lodash';
 function List(props) {
   const [list, setList] = useState(props.menuitems);
 
-  // Function to handle removal of an item by its index
   function handleRemove(index) {
     const newList = list.filter((_, i) => i !== index);
     setList(newList);
   }
-  
+
+
   let listOrder = _.orderBy(list, ['name'], ['asc']);
 
-  // Map through the list and create an array of elements
+
   const listItems = list.map((name, index) => (
     
     <li key={index} className='li'>
@@ -24,6 +24,8 @@ function List(props) {
       
     </li>
   ));
+  
+ 
 
   return (
     <ol>
